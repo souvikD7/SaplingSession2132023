2e46d9850307a4d5436719eadb528ae57eb7d8cd
Souvik Dutta <126692500+souvikD7@users.noreply.github.com>
2023-03-21 17:35:53 +0530

    Add files via upload

---
+++ swinglearning.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class swinglearning {
+    public static void main(String[] args) {
+        JFrame frame= new JFrame("Sapling Session");
+        frame.setVisible(true);
+    }
+    
+}
